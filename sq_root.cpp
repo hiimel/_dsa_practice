224bf52b49d72bbd51dc0b92415f9597e1d02643
hiimel <himel.browser@gmail.com>
2026-02-04 10:03:53 +0600

    practice

---
+++ sq_root.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+
+    double method1 = exp(0.5 * (log(n)));
+    cout << method1 << endl;
+
+    return 0;
+}
