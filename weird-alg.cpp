210accbd5c8d53b2db245ee39b14015a055cac4c
hiimel <himel.browser@gmail.com>
2026-02-15 11:24:51 +0600

    according to the question

--- weird-alg.cpp
+++ weird-alg.cpp
@@ -8,13 +8,13 @@ int main() {
     cin >> n;
 
     while(n != 1) {
+        cout << n << " ";
         if(n / 2 * 2 == n) {
             n = n / 2;
         } else {
             n = n * 3 + 1;
         }
 
-        cout << n << " ";
     }
 
     cout << endl;
