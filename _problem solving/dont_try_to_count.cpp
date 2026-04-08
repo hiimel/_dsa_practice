46a9c6c39ea3a22e5458dc9ee9bdca8a29fd1221
hiimel <himel.browser@gmail.com>
2026-04-08 19:47:24 +0600

    rating problems

---
+++ _problem solving/dont_try_to_count.cpp
@@ -0,0 +1,60 @@
+// Link: https://codeforces.com/problemset/problem/1881/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isCheck(string s, string x) {
+    if(x.length() < s.length()) {
+        return false;
+    }
+
+    for(int i = 0; i < x.length() - s.length() + 1; i++) {
+        if(x.substr(i, s.length()) == s) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        long long n, m;
+        cin >> n >> m;
+
+        string x;
+        cin >> x;
+
+        string s;
+        cin >> s;
+
+        string x0 = x;
+        string x1 = x0 + x0;
+        string x2 = x1 + x1;
+        string x3 = x2 + x2;
+        string x4 = x3 + x3;
+        string x5 = x4 + x4;
+
+        long long ans = -1;
+        if(isCheck(s, x0)) {
+            ans = 0;
+        } else if(isCheck(s, x1)) {
+            ans = 1;
+        } else if(isCheck(s, x2)) {
+            ans = 2;
+        } else if(isCheck(s, x3)) {
+            ans = 3;
+        } else if(isCheck(s, x4)) {
+            ans = 4;
+        } else if(isCheck(s, x5)) {
+            ans = 5;
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
