f6dde8dbd7c534dbbe2dc804fed6a969c3019263
hiimel <himel.browser@gmail.com>
2026-04-17 22:51:54 +0600

    rating string problems

---
+++ _problem solving/translation.cpp
@@ -0,0 +1,21 @@
+// Link: https://codeforces.com/problemset/problem/41/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    string t;
+    cin >> t;
+
+    reverse(s.begin(), s.end());
+    if(s == t) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
