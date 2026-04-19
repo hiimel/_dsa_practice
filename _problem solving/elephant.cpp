bf3ac2ab9881a33f91f0098298f1dd905e0ef817
hiimel <himel.browser@gmail.com>
2026-04-19 20:17:41 +0600

    rating problems

---
+++ _problem solving/elephant.cpp
@@ -0,0 +1,13 @@
+// Link: https://codeforces.com/problemset/problem/617/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double x;
+    cin >> x;
+
+    double res = x / 5;
+    cout << ceil(res) << endl;
+    return 0;
+}
