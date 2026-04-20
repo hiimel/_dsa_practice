1f17c37959744566c16501ad61a0e1f40767d284
hiimel <himel.browser@gmail.com>
2026-04-20 19:15:08 +0600

    rating problems

---
+++ _problem solving/stones_on_the_table.cpp
@@ -0,0 +1,22 @@
+// Link: https://codeforces.com/problemset/problem/266/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int sameStones = 0;
+    for(int i = 0; i < s.length(); i++) {
+        if(s[i] == s[i + 1]) {
+            sameStones++;
+        }
+    }
+
+    cout << sameStones << endl;
+    return 0;
+}
