625bb2f534dbb1b85994f7062484528209d2541b
hiimel <himel.browser@gmail.com>
2026-07-21 19:17:47 +0600

    solved

--- practiceArrays/buy_sell.cpp
+++ practiceArrays/buy_sell.cpp
@@ -3,24 +3,20 @@
 using namespace std;
 
 void  stocks(int arr[], int n) {
-    int buy[100000];
-    buy[0] = INT_MAX;
+    int minPrices = arr[0];
+    int maxProfit = 0;
 
     for(int i = 1; i < n; i++) {
-        buy[i] = min(buy[i  - 1], arr[i - 1]);
+        int profit = arr[i] -  minPrices;
+        maxProfit = max(maxProfit, profit);
+        minPrices = min(minPrices, arr[i]);
     }
 
-    int max_profit = 0;
-    for(int i =  0; i < n; i++) {
-        int curren_profit = arr[i] - buy[i];
-        max_profit = max(max_profit, curren_profit);
-    }
-
-    cout << max_profit << endl;
+    cout <<  maxProfit <<  endl;
 }
 
 int main() {
-    int prices[] = {7, 1, 5, 3, 6, 4};
+    int prices[] = {1, 3, 6, 9, 11};
     int n = sizeof(prices) / sizeof(prices[0]);
 
     stocks(prices, n);
