fd85e10709067ee31c1fada7b904023b0a78a336
hiimel <himel.browser@gmail.com>
2026-08-21 08:23:12 +0600

    solved

--- Max_consecutive_ones.cpp
+++ Max_consecutive_ones.cpp
@@ -1,28 +1,35 @@
+//Link: https://leetcode.com/problems/max-consecutive-ones/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int findMaxConsecutiveOnes(vector<int> &nums) {
-    int count = 0; 
-    int maxCount = 0;
+    int cntOnes = 0;
+    int maxCnt = 0;
 
     for(int i = 0; i < nums.size(); i++) {
         if(nums[i] == 1) {
-            count++;
+            cntOnes++;
         } else {
-            maxCount = max(maxCount, count);
-            count = 0;
+            cntOnes = 0;
         }
+
+        maxCnt = max(maxCnt, cntOnes);
     }
 
-    maxCount = max(maxCount, count);
-
-    return maxCount;
+    return maxCnt;
 }
 
 int main() {
-    vector<int> a = {1,0,1,1,0,1};
+    int n;
+    cin >> n;
 
-    cout << findMaxConsecutiveOnes(a) << endl;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << findMaxConsecutiveOnes(arr) << endl;
 
     return 0;
 }
--- Third_max_number.cpp
+++ Third_max_number.cpp
@@ -1,27 +1,40 @@
+//Link: https://leetcode.com/problems/third-maximum-number/description/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int thirdMax(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
+    long long firstEl = LLONG_MIN, secondEl = LLONG_MIN, thirdEl = LLONG_MIN;
 
-    int cnt = 1;
-    for(int i = nums.size() - 2; i >= 0; i--) {
-        if(nums[i] != nums[i + 1]) {
-            cnt++;
-        }
-
-        if(cnt == 3) {
-            return nums[i];
+    for(int i = 0; i < nums.size(); i++) {
+        if(nums[i] > firstEl) {
+            thirdEl = secondEl;
+            secondEl = firstEl;
+            firstEl = nums[i];
+        } else if(nums[i] > secondEl && nums[i] != firstEl) {
+            thirdEl = secondEl;
+            secondEl = nums[i];
+        } else if(nums[i] > thirdEl && nums[i] != secondEl && nums[i] != firstEl) {
+            thirdEl = nums[i];
         }
     }
 
-    return nums[nums.size() - 1];
+    if(thirdEl == LLONG_MIN) {
+        return firstEl;
+    } else {
+        return thirdEl;
+    }
 }
 
 int main() {
-    vector<int> a = {5, 2, 2};
+    int n;
+    cin >> n;
 
-    cout << thirdMax(a) << endl;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
+    cout << thirdMax(arr) << endl;
     return 0;
 }
